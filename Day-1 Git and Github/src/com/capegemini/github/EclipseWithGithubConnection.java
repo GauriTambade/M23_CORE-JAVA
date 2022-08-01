3807cb0800061a2d4e5b14baeb3beacf5358ede8
lenovo <lenovo@DESKTOP-BN61AEV>
2022-08-01 23:37:26 +0530

    first commit

---
+++ Day-1 Git and Github/src/com/capegemini/github/EclipseWithGithubConnection.java
@@ -0,0 +1,11 @@
+package com.capegemini.github;
+//demo for connection of Eclipse with Github
+public class EclipseWithGithubConnection {
+
+	public static void main(String[] args) {
+		//print the line
+		System.out.println("Welcome to C2TC program");
+
+	}
+
+}
