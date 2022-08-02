40c9984977a1483a4d464a3ddcf9d7c1e01d2486
lenovo <lenovo@DESKTOP-BN61AEV>
2022-08-02 15:45:25 +0530

    second commit

---
+++ Day 2- Introduction to Java/src/com/capegemini/javaintro/ByteProgram.java
@@ -0,0 +1,13 @@
+package com.capegemini.javaintro;
+
+public class ByteProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    byte a=127;
+    //byte a=129;
+    //give error because  byte range
+    System.out.println(a);
+	}
+
+}
