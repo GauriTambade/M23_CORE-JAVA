e03004be0b76412645874009a6af69bc6ae51efe
lenovo <lenovo@DESKTOP-BN61AEV>
2022-08-12 14:09:26 +0530

    second commit

---
+++ Day 8 Polymorphism/src/com/cg/polymorphism/CompileTimePolymorphism.java
@@ -0,0 +1,30 @@
+package com.cg.polymorphism;
+//program on method overloading
+class Shape
+{
+	//by changing the number of arguments
+	int area(int a)
+	{
+		
+		return a*a;
+	}
+	int area(int a, int b)
+	{
+		return a*b;
+		
+	}
+}
+
+
+
+public class CompileTimePolymorphism {
+
+	public static void main(String[] args) {
+	Shape s=new Shape();
+	System.out.println(s.area(5));
+	System.out.println(s.area(7, 9));
+	
+
+	}
+
+}
