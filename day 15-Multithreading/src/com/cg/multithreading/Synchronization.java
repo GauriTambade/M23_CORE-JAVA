d230a45a165f70acdd1666ce2719cc7318dd8777
lenovo <lenovo@DESKTOP-BN61AEV>
2022-08-22 09:55:01 +0530

    first commit

---
+++ day 15-Multithreading/src/com/cg/multithreading/Synchronization.java
@@ -0,0 +1,26 @@
+package com.cg.multithreading;
+class A
+{
+	void print(int var)
+	{
+		synchronized(this)
+		{
+			for(int i=1;i<=3;i++)
+			{
+				System.out.println(var*i);
+				try
+				{
+					Thread
+				}
+			}
+		}
+	}
+}
+public class Synchronization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
